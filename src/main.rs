e03ef053fa1ed947e95e6bbf435970df04f70c99
Yiwei Lin <s921975628@gmail.com>
2024-05-09 00:31:17 +0800

    Allow memwatch to trace kernel symbols

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io::Error;
 use std::mem::size_of;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -12,8 +13,10 @@ use libbpf_rs::RingBufferBuilder;
 use libbpf_rs::{skel::*, Link, Program};
 
 use anyhow::{anyhow, Result};
-use libc::getchar;
-use perf_event_open_sys::bindings::{perf_event_attr, HW_BREAKPOINT_RW, PERF_TYPE_BREAKPOINT};
+use libc::{c_int, getchar, pid_t};
+use perf_event_open_sys::bindings::{
+    perf_event_attr, HW_BREAKPOINT_RW, HW_BREAKPOINT_X, PERF_TYPE_BREAKPOINT,
+};
 use perf_event_open_sys::perf_event_open;
 use plain::Plain;
 
@@ -49,24 +52,53 @@ fn load_ebpf_prog() -> Result<MemwatchSkel<'static>> {
     open_skel.load().map_err(anyhow::Error::msg)
 }
 
-fn attach_breakpoint(symbol_addr: usize, prog: &mut Program) -> Result<Link> {
-    let mut attr = perf_event_attr::default();
-    attr.size = size_of::<perf_event_attr>() as u32;
-    attr.type_ = PERF_TYPE_BREAKPOINT;
-    attr.__bindgen_anon_3.bp_addr = symbol_addr as u64;
-    attr.__bindgen_anon_4.bp_len = 4; // 4 bytes breakpoint
-    attr.bp_type = HW_BREAKPOINT_RW;
-    attr.__bindgen_anon_1.sample_period = 1; // response to every event
-    attr.__bindgen_anon_2.wakeup_events = 1;
-    attr.set_precise_ip(2); // request synchronous delivery
+fn attach_perf_event(
+    attr: &mut perf_event_attr,
+    pid: pid_t,
+    cpu: c_int,
+    group_fd: c_int,
+    prog: &mut Program,
+) -> Result<Link> {
+    let efd = unsafe {
+        perf_event_open(
+            attr as *mut perf_event_attr,
+            pid,
+            cpu,
+            group_fd,
+            PERF_FLAG_FD_CLOEXEC as u64,
+        )
+    };
 
-    let efd = unsafe { perf_event_open(&mut attr, 0, -1, -1, PERF_FLAG_FD_CLOEXEC as u64) };
     if efd < 0 {
-        return Err(anyhow!("perf_event_open() fail"));
+        println!("efd = {}", efd);
+        return Err(anyhow!(format!(
+            "perf_event_open() fail: {}",
+            Error::last_os_error()
+        )));
     }
 
     let link = prog.attach_perf_event(efd)?;
     Ok(link)
+}
+
+fn attach_breakpoint(symbol_addr: usize, prog: &mut Program) -> Result<Vec<Link>> {
+    let mut attr = perf_event_attr::default();
+    attr.size = size_of::<perf_event_attr>() as u32;
+    attr.type_ = PERF_TYPE_BREAKPOINT;
+    attr.__bindgen_anon_3.bp_addr = symbol_addr as u64;
+    attr.__bindgen_anon_4.bp_len = 8; // 4 bytes breakpoint
+    attr.bp_type = HW_BREAKPOINT_X;
+    attr.__bindgen_anon_1.sample_period = 1; // response to every event
+    attr.__bindgen_anon_2.wakeup_events = 1;
+    attr.set_precise_ip(2); // request synchronous delivery
+
+    let mut links = Vec::new();
+    for cpu in get_online_cpus() {
+        let link = attach_perf_event(&mut attr, -1, cpu, -1, prog)?;
+        links.push(link);
+    }
+
+    Ok(links)
 }
 
 #[repr(C)]
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,31 @@
+use libc::c_int;
+use std::fs;
+
 pub fn cast<T: plain::Plain>(args: &[u8]) -> &T {
     let size = std::mem::size_of::<T>();
     let slice = &args[0..size];
     return plain::from_bytes::<T>(slice).expect("Fail to cast bytes");
+}
+
+pub fn get_online_cpus() -> Vec<c_int> {
+    let path = "/sys/devices/system/cpu/online";
+
+    let list = fs::read_to_string(path).expect("Fail to read cpu/online sysfs node");
+
+    let list = list.trim().split(",");
+
+    let mut cpus = Vec::new();
+    for range in list {
+        if let Some(pos) = range.find('-') {
+            let start = range[0..pos].parse::<c_int>().unwrap();
+            let end = range[pos + 1..].parse::<c_int>().unwrap();
+            for c in start..=end {
+                cpus.push(c);
+            }
+        } else {
+            cpus.push(range.parse::<c_int>().unwrap());
+        }
+    }
+
+    cpus
 }
