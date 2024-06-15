c18584e7b386d8eb7e1bf299771eb9e3f89ab736
Yiwei Lin <s921975628@gmail.com>
2024-06-15 13:42:45 +0800

    Change cmdline interface to drgn::task

--- tools/task.py
+++ tools/task.py
@@ -1,13 +1,19 @@
-import sys
+import argparse
 
 import drgn
 from drgn.helpers.common import *
 from drgn.helpers.linux import *
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pid", type=int, help="pid of the task_struct")
+    args = parser.parse_args()
+    return args
+
 if len(sys.argv) < 2:
     exit(f"usage: {sys.argv[0]} pid")
 
-pid = int(sys.argv[1])
+pid = get_args().pid
 
 task = find_task(pid)
 if not task:
