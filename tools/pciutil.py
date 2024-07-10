87bac2fb271194ff5262e089d602e4cbdf4926b3
RinHizakura <s921975628@gmail.com>
2024-07-10 17:02:33 +0800

    Add wrapper utils for pci_dev

---
+++ tools/pciutil.py
@@ -0,0 +1,6 @@
+from drgn import cast
+
+def pci_get_drvdata(typ, pdev):
+    # Cast driver_data to the given type. For example:
+    # pci_get_drvdata("struct net_device *", pdev)
+    cast(typ, pdev.dev.driver_data)
