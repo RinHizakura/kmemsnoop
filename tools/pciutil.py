2f56dcdebddb3a4ec305fc6ffbe23e4489cd7284
Yiwei Lin <s921975628@gmail.com>
2024-08-16 00:00:48 +0800

    Remove explaination of using drgn to find address
    The explaination to use drgn may be too complicate, we just
    intergrate everything as "kexpr" in the tool

--- tools/pciutil.py
+++
@@ -1,6 +0,0 @@
-from drgn import cast
-
-def pci_get_drvdata(typ, pdev):
-    # Cast driver_data to the given type. For example:
-    # pci_get_drvdata("struct net_device *", pdev)
-    return cast(typ, pdev.dev.driver_data)
